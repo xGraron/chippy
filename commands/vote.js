31853bed1edbbee67125fa8097a037d13babaee8
xGraron <xgraron@gmail.com>
2026-08-25 17:39:49 +0200

    Issue #85, added missing requirements

--- commands/vote.js
+++ commands/vote.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js")	
+const { SlashCommandBuilder, EmbedBuilder, MessageAttachment, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js")
 const dev   = require('../handlers/dev.js')
 
 module.exports = 
