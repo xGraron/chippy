8d782eba954bacb9ffb20942203f60a4578d9ade
xGraron <xgraron@gmail.com>
2026-08-28 14:32:08 +0200

    bugfix

--- handlers/responseHandler.js
+++ handlers/responseHandler.js
@@ -7,14 +7,14 @@ async function respond(interaction, logIDs)
 {
     const button = new ButtonBuilder()
     .setLabel("Discord server")
-    .setURL("")
+    .setURL("https://www.discord.com/")
     .setStyle(ButtonStyle.Link)
 
     const row = new ActionRowBuilder().addComponents(button)
 
     const embed = new EmbedBuilder()
     .setTitle(`${interaction.user.displayName}, hey there!`)
-    .setDescription(`The people have spoken! **Chippy *will* return** \n Work is already beginning 👀 \n`)
+    .setDescription(`The people have spoken, **Chippy *will* return**! \n Work is already beginning 👀 \n`)
     .setFooter({ text: `Please keep in mind that this will take time!` })
 
     try     { await interaction.editReply({ embeds: [embed] }) }
