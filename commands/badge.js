a2d6f5636251e6d763e477c7a77edbfbaf15a7d8
xGraron <xgraron@gmail.com>
2026-08-28 14:32:32 +0200

    issue #87 and #88

--- commands/badge.js
+++ commands/badge.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder, MessageAttachment, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js")
+const { respond }    = require('../handlers/responseHandler.js')
 const dev   = require('../handlers/dev.js')
 
 module.exports = 
@@ -11,20 +12,6 @@ module.exports =
 	{
         await interaction.deferReply()
 
-        const button = new ButtonBuilder()
-        .setLabel("Take the survey!")
-		.setURL("https://forms.gle/PNerFHLpQb7ha9NE9")
-        .setStyle(ButtonStyle.Link)
-
-        const row = new ActionRowBuilder().addComponents(button)
-
-        const embed = new EmbedBuilder()
-        .setTitle(`${interaction.user.displayName}, hey there!`)
-        .setDescription(`Chippy has reached EoL a while ago, but I am currently **planning to revive it!** \n
-        However, for that to happen **I need your help.** \n`)
-        .setFooter({ text: `Please take two minutes to fill out this survey!` })
-
-        try     { await interaction.editReply({ embeds: [embed], components: [row] }) }
-        catch   { dev.log("Failed to respond \n cmdID: 8, Error: 1", 2) }
+        respond(interaction, [8, 1])
     }
 }
